4394ad3a3d8a22925858868978f112b255a03811
o d <onurdemirhan@gmail.com>
2023-02-18 00:13:04 +0300

    duzenleme yapildi

--- fiyatlar.py
+++ fiyatlar.py
@@ -1,80 +1,127 @@
 from bs4 import BeautifulSoup
 import requests
+import tweepy
+
+headers = {
+    'User-Agent':
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0"
+}
 
 
 def cimri(webpage):
-    headers = {
-        'User-Agent':
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0"
-    }
-    with requests.get(webpage, headers=headers) as url:
+    with requests.get(webpage["url"],
+                      params=webpage.get("params"),
+                      headers=headers) as url:
         data = url.content
     soup = BeautifulSoup(data, "html.parser")
-    product_elements = soup.find_all(id="cimri-product")
+    product_elements = soup.find(class_="cACjAF")
+    product_elements = product_elements.find_all(id="cimri-product")
     product_prices = {}
     for element in product_elements:
-        product_title = element.contents[0].find_all(
-            class_="link-detail")[0].attrs["title"]
-        product_price = element.contents[0].find_all(
-            class_="top-offers")[0].contents[0].next_element.nextSibling
-        product_prices[product_title] = product_price
+        try:
+            # product = element.contents[0].find(class_="cACjAF")
+            product_title = element.contents[0].find_all(
+                class_="link-detail")[0].attrs["title"]
+            product_price = float(
+                element.contents[0].find_all(class_="top-offers")[0].
+                contents[0].next_element.nextSibling.split(" TL")[0].replace(
+                    ".", "").replace(",", "."))
+            product_prices[product_title] = (product_price)
+        except (IndexError, AttributeError) as error:
+            print(error)  # handle error
+    product_prices = dict(
+        sorted(product_prices.items(), key=lambda x: x[1])[:2])
     return product_prices
 
 
 def akakce(webpage):
-    headers = {
-        'User-Agent':
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0"
-    }
-    with requests.get(webpage, headers=headers) as url:
+    with requests.get(webpage["url"],
+                      params=webpage.get("params"),
+                      headers=headers) as url:
         data = url.content
     soup = BeautifulSoup(data, "html.parser")
-    product_elements = soup.find_all("li")
+    # product_elements = soup.find_all("li")
+    product_elements = soup.find(id="APL").find_all("li")
     product_prices = {}
     for element in product_elements:
         try:
             product_title = element.find_all(class_="pn_v8")[0].text
-            product_price = element.find_all(
-                class_="pt_v9")[0].text.split("TL")[0].strip()
+            product_price = float(
+                element.find_all(
+                    class_="pt_v9")[0].text.split("TL")[0].strip().replace(
+                        ".", "").replace(",", "."))
             product_prices[product_title] = product_price
-        except IndexError:
-            pass  # handle error
+        except (IndexError, AttributeError) as error:
+            print(error)  # handle error
+    product_prices = dict(
+        sorted(product_prices.items(), key=lambda x: x[1])[:2])
     return product_prices
+
 
 def epey(webpage):
-    headers = {
-        'User-Agent':
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0"
-    }
-    with requests.get(webpage, headers=headers) as url:
+    with requests.get(webpage["url"],
+                      params=webpage.get("params"),
+                      headers=headers) as url:
         data = url.content
     soup = BeautifulSoup(data, "html.parser")
-    product_elements = soup.find_all("li")
+    product_elements = soup.find_all(class_="listele table")[0].find_all("ul")
     product_prices = {}
     for element in product_elements:
         try:
-            product_title = element.find_all(class_="pn_v8")[0].text
-            product_price = element.find_all(
-                class_="pt_v9")[0].text.split("TL")[0].strip()
+            product_title = element.find(class_="urunadi").text
+            if element.find(class_="fiyat cell").find("a") is not None:
+                product_price = float(
+                    element.find(class_="fiyat cell").find(
+                        "a").next_element.split("TL")[0].strip().replace(
+                            ".", "").replace(",", "."))
             product_prices[product_title] = product_price
-        except IndexError:
-            pass  # handle error
+            sorted(product_prices.items(), )
+        except (IndexError, AttributeError) as error:
+            print(error)  # handle error
+    product_prices = dict(
+        sorted(product_prices.items(), key=lambda x: x[1])[:2])
     return product_prices
 
 
+query = "Radeon RX 6750 XT"
+
+WEBSITES = {
+    "cimri": {
+        "url": "https://www.cimri.com/arama",
+        "params": {
+            "sort": "price,asc",
+            "q": query
+        },
+    },
+    "akakce": {
+        "url": "https://www.akakce.com/arama/",
+        "params": {
+            "q": query,
+            "s": "2"
+        },
+    },
+    "epey": {
+        "url":
+        "https://www.epey.com/ekran-karti/grafik-islemcisi/" +
+        query.replace(" ", "-"),
+        "params":
+        ""
+    },
+}
+
+# cimri_webpage = "https://www.cimri.com/arama?sort=price%2Casc&q=radeon+6750+xt"
+# akakce_webpage = "https://www.akakce.com/arama/?q=6750+XT&s=2"
+# epey_webpage = "https://www.epey.com/ekran-karti/grafik-islemcisi/radeon-rx-6750-xt"
 
 
 def main():
-    cimri_webpage = "https://www.cimri.com/arama?sort=price%2Casc&q=radeon+6800+xt"
-    akakce_webpage = "https://www.akakce.com/arama/?q=6750+XT&s=2"
-    cimri_prices = cimri(cimri_webpage)
-    akakce_prices = akakce(akakce_webpage)
-    print("\n cimri BASLIYOR")
-    for title, price in cimri_prices.items():
-        print(title, "@", price)
-    print("\n akakce BASLIYOR ")
-    for title, price in akakce_prices.items():
-        print(title, "@", price)
+    prices = {}
+    for website in WEBSITES:
+        func = globals()[website]
+        prices[website + "_prices"] = func(WEBSITES[website])
+    for website in prices:
+        for item, price in prices[website].items():
+            print(f"{item}: {price} @{website}")
 
 
 if __name__ == '__main__':
